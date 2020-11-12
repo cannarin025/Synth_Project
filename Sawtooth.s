b7e0f142b4880c738ff26c85f6c11bd45e20c987
James <jamescameronmilne@gmail.com>
2020-11-12 11:10:19 +0000

    sawtooth made, keypad fixed.

--- Sawtooth.s
+++ Sawtooth.s
@@ -1,20 +1,10 @@
 #include <xc.inc>
- 
-global Keypad_Init, Keypad_Loop
+    
+global sawloop
     
 psect    udata_acs	    ; named variables in access ram
 sawval:	    ds 1    ; reserve 1 byte for current counter val
-sawmax:	    ds 1
-sawmin:	    ds 1
 
-
-
-
-delay_count1:	    ds 1    ; reserve 1 byte for counting
-delay_count2:	    ds 1    ; reserve 1 byte for counting
-delay_count3:	    ds 1    ; reserve 1 byte for counting
-wtemp:		    ds 1    ; reserve 1 byte for temp w val
-    
 shortdelay:	    ds 1    ; reserve 1 byte for the short delay countdown
     
 psect    Keypad_code, class=CODE
@@ -22,7 +12,7 @@ psect    Keypad_code, class=CODE
 sawloop:
     incf    sawval, A
     call    delay
-  
+    bra	    sawloop
 
 delay:
     movlw   0xFF
@@ -32,3 +22,4 @@ delay_loop:
     decfsz  shortdelay, A
     bra	    delay_loop
     return
+    
--- main.s
+++ main.s
@@ -1,8 +1,8 @@
 	#include <pic18_chip_select.inc>
 	#include <xc.inc>
-
-extrn	Keypad
 	
+extrn sawloop	
+
 psect	code, abs
 	
 main:
@@ -12,5 +12,7 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	
+    call	sawloop
+    goto	$
     
 	end main
