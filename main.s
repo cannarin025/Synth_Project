7cd4afb0fb4133d2120a57a5993e27fe2a07e405
James <jamescameronmilne@gmail.com>
2020-11-17 09:59:04 +0000

    interrupts started

--- main.s
+++ main.s
@@ -6,13 +6,22 @@ extrn sawloop
 psect	code, abs
 	
 main:
-	org	0x0
-	goto	start
+    org	0x0
+    goto	start
 
-	org	0x100		    ; Main code starts here at address 0x100
+    org	0x100		    ; Main code starts here at address 0x100
+
+int_hi:
+    org	0x0008
+    goto	intchk_hi
+	
+;int_lo:
+;    org	0x0018
+;    goto	intchk_lo
+	
 start:
 	
-    call	sawloop
+    call	Keypad_Loop
     goto	$
     
-	end main
+    end main
