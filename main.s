2e58d9743cdc81fea955ad14bfb861431fbbc27b
James <jamescameronmilne@gmail.com>
2020-11-23 18:01:28 +0000

    check_nonote added to interrupt and keypad structure changed

--- main.s
+++ main.s
@@ -1,13 +1,13 @@
 #include <xc.inc>
-	extrn PWM_setup, PWM_set_note, PWM_play_note, PWM_stop_note, Keypad_Init, Keypad_Loop, CCP5_Setup, CCP5_Enable_Timer, CCP5_Disable_Timer, CCP6_Setup, CCP6_Enable_Timer, CCP6_Disable_Timer, CCP5_Int_Hi, CCP6_Int_Hi
+    extrn PWM_setup, PWM_set_note, PWM_play_note, PWM_stop_note, Keypad_Init, Keypad_Loop, CCP5_Setup, CCP5_Enable_Timer, CCP5_Disable_Timer, CCP6_Setup, CCP6_Enable_Timer, CCP6_Disable_Timer, CCP5_Int_Hi, CCP6_Int_Hi
 
-	psect	code, abs
-	
+    psect    code, abs
+    
 main:
-    org	0x0
-    goto	start
+    org    0x0
+    goto    start
 
-	org	0x100		    ; Main code starts here at address 0x100
+    org    0x100            ; Main code starts here at address 0x100
 
 int_hi:
     org 0x0008
@@ -20,12 +20,12 @@ start:
     clrf    LATH, A
     clrf    LATD, A
     ;call   PWM_setup
-    ;call   Keypad_Init
+    call    Keypad_Init
     call    CCP5_Setup
     call    CCP6_Setup
-    call    CCP5_Enable_Timer
-    call    CCP6_Enable_Timer
+    ;call    CCP5_Enable_Timer
+    ;call    CCP6_Enable_Timer
 loop:
-    ;call   Keypad_Loop
-    bra	    loop
+    call    Keypad_Loop
+    bra        loop
     goto $
