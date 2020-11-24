c4680a768ca159b392335d45c8db22ae85792a83
Can Narin <cannarin025@gmail.com>
2020-11-24 08:50:42 +0000

    Merge origin/CannoBranch into CannoBranch
    Conflicts:
    	CCP.s
    	CCP_Interrupt.s
    	Keypad.s
    	Synth_Keypad.s
    	main.s

--- Tables.s
+++ Tables.s
@@ -8,7 +8,7 @@ counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+sinArray:    ds 0x80 ; reserve 128 bytes for message data
 
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
@@ -27,13 +27,13 @@ setup:	bcf	CFGS	; point to Flash program memory
 	
 	; ******* Main programme ****************************************
 start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
+	movlw	low highword(sinTable)	; address of data in PM
 	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
+	movlw	high(sinTable)	; address of data in PM
 	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
+	movlw	low(sinTable)	; address of data in PM
 	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
+	movlw	sinTable_l	; bytes to read
 	movwf 	counter, A		; our counter register
 loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
